400fcd7a8ab7989dbf3cfca0a499999c69d042ca
bakare <bakaredavid007@gmail.com>
2022-11-16 04:14:16 +0100

    test 2, task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -22,7 +22,7 @@ void print_num(int n)
 
 void binary_tree_preorder(const binary_tree_t *tree, void(*func)(int))
 {
-	if (tree != NULL)
+	if (tree != NULL && func != NULL)
 	{
 		func(tree->n);
 		binary_tree_preorder(tree->left, func);
