c59531a7e0ec60d29ffa89ed61e010ac443d34f0
3akare <bakaredavid007@gmail.com>
2022-11-16 04:12:07 +0100

    test 1, task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * print_num - Prints a number
+ *
+ * @n: Number to be printed
+ */
+
+void print_num(int n)
+{
+	printf("%d\n", n);
+}
+
+
+/**
+ * binary_tree_preorder - Goes through a binary tree
+ * using the pre-order traversal
+ *
+ * @func: A pointer to the root node of the tree
+ * @tree: A pointer to the root node of the tree to traverse
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void(*func)(int))
+{
+	if (tree != NULL)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
--- binary_trees.h
+++ binary_trees.h
@@ -37,5 +37,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *);
+void binary_tree_preorder(const binary_tree_t *tree, void(*func)(int));
+void print_num(int n);
 
 #endif /* _BINARY_TREES_H_ */
