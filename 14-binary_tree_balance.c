6f8c2ecb688f7bca83e80c2702f4a42b76145188
bakare <bakaredavid007@gmail.com>
2022-11-17 23:08:11 +0100

    readme update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,8 +11,8 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left = binary_tree_height(tree->left);
-	int right = binary_tree_height(tree->right);
+	int left = (int) binary_tree_height(tree->left);
+	int right = (int) binary_tree_height(tree->right);
 
 	return (left - right);
 }
